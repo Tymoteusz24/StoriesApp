a6045247402e8c15476073e199c110769793bb07
Tymoteusz Pasieka <tymoteusz.pasieka@gmail.com>
2025-10-22 08:55:27 +0200

    Initial Commit

---
+++ TheDatingAppTests/TheDatingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheDatingAppTests.swift
+//  TheDatingAppTests
+//
+//  Created by Tymoteusz Pasieka on 10/22/25.
+//
+
+import Testing
+@testable import TheDatingApp
+
+struct TheDatingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
